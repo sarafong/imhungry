c94842862515fa3456ef8278faba9cfdb0997ff8
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-02-24 17:00:28 -0500

    Converted all javascript in HTML to a seperate javascript file.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//------load maps-API
+window.addEventListener('load',function(){
+
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAQ82u9EX5v0IX5fQAKBWqikHWUWZv1rN0&callback=initMap';
+  document.body.appendChild(script);
+});
+
 const latitude = 43.8561;
 const longitude = -79.3370;
 function initMap(){
