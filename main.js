2a52f4f269cf4f5613c4a46ac683899b793ea01a
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-02-25 10:59:44 -0500

    Used firebase to update main.js

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 //------load maps-API
 window.addEventListener('load',function(){
-
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAQ82u9EX5v0IX5fQAKBWqikHWUWZv1rN0&callback=initMap';
     document.body.appendChild(script);
 });
+var latitude=5;
+var longitude=5;
 
+firebase.database().ref().on('value',function(snapshot){
+  latitude = (snapshot.val().Latitude);
+  longitude = (snapshot.val().Longitude);
+  initMap();
+});
 function initMap(){
+  setTimeout(function(){
     //map location
-    var myLoc = {lat: 25, lng: 35};
+    console.log(latitude);
+    console.log(longitude);
+    longitude = parseFloat(longitude);
+    latitude = parseFloat (latitude);
+    var myLoc = {lat: latitude, lng: longitude};
     //map options
     var options = {
-        zoom:10,
+        zoom:15,
         center: myLoc
     };
     //new map
@@ -22,4 +33,6 @@ function initMap(){
         position: myLoc,
         map: map
     });
+
+  }, 1000);
 }
