1a756b4c2ae9fbe15a8300f67637fb3e8b602614
Sara Fong <sarafong19@gmail.com>
2018-02-25 06:30:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+const Alexa = require('alexa-sdk');
+var AWS = require('aws-sdk');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = undefined;
+
+const SKILL_NAME = 'Space Facts';
+const WELCOME_MESSAGE = 'what up';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
+//=========================================================================================================================================
+const data = [
+    'A year on Mercury is just 88 days long.',
+    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
+    'On Mars, the Sun appears about half the size as it does on Earth.',
+    'Earth is the only planet not named after a god.',
+    'Jupiter has the shortest day of all the planets.',
+    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+    'The Sun contains 99.86% of the mass in the Solar System.',
+    'The Sun is an almost perfect sphere.',
+    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+    'The temperature inside the Sun can reach 15 million degrees Celsius.',
+    'The Moon is moving approximately 3.8 cm away from our planet every year.'
+];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.response.speak("Tell me about your day");
+      // this.emit('GetNewFactIntent');
+    },
+    'Unhandled': function () {
+        console.log(this.event.request);
+        console.log("unhandled");
+        this.response.speak('unhandled error');
+    },
+    'GetNewFactIntent': function () {
+        const factArr = data;
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+    //   const speechOutput = GET_FACT_MESSAGE + randomFact;
+  const speechOutput = WELCOME_MESSAGE;
+ //       this.response.cardRenderer(SKILL_NAME, randomFact);
+   //     this.response.speak(speechOutput);
+       //  this.emit(':responseReady');
+        var myDay = "I'm feeling"+ this.event.request.intent.slots.MOOD.value;
+        var lambda = new AWS.Lambda({region: 'us-east-1', apiVersion: '2015-03-31'});
+        var pullParams = {
+            FunctionName : 'hungrypythonlambda',
+            InvocationType : 'RequestResponse',
+            LogType : 'None',
+            Payload : JSON.stringify({'mood': myDay})
+        };
+        var self = this;
+        lambda.invoke(pullParams, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                var res = JSON.parse(data.Payload);
+                console.log(res);
+                //self.emit(':tell', "Your mood is " + myDay);
+                self.emit(':tell', res);
+            }
+        });   
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
